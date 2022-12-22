8d6c4a9ad61f5e8870f6b7d45158608c2c04f034
rwishd17 <rwishd17@gmail.com>
2022-12-22 14:08:11 +0300

    Add Api Mars

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
@@ -22,7 +22,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.android.marsphotos.PhotoGridAdapter
 import com.example.android.marsphotos.databinding.FragmentOverviewBinding
+import com.example.android.marsphotos.databinding.GridViewItemBinding
 
 /**
  * This fragment shows the the status of the Mars photos web services transaction.
@@ -40,6 +42,10 @@ class OverviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentOverviewBinding.inflate(inflater)
+
+//        val binding = GridViewItemBinding.inflate(inflater)
+
+        binding.photosGrid.adapter = PhotoGridAdapter()
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.lifecycleOwner = this
